b0ad246c9204b946f2ed3398cca9ab6161cce1d3
namanjain06 <Njain@199706>
2025-10-16 16:47:46 +0530

    VariableDeclarationAndInitialization

---
+++ src/CodingChallenge65/VariableDeclarationAndInitialization.java
@@ -0,0 +1,15 @@
+package CodingChallenge65;
+
+public class VariableDeclarationAndInitialization {
+    public static void main(String[] args) {
+        int studentAge = 20;
+        double productPrice = 299.99;
+        boolean isStudentActive = true;
+        char gradeLevel = 'A';
+
+        System.out.println("Student Age: " + studentAge);
+        System.out.println("Product Price $: " + productPrice);
+        System.out.println("Is Student Active: " + isStudentActive);
+        System.out.println("Grade Level: " + gradeLevel);
+    }
+}

615809dfee6bcb1de83a01e33233b7e3d9e76b5b
namanjain06 <Njain@199706>
2025-10-16 23:40:59 +0530

    PrintNumbers

---
+++ src/CodingChallenge65/PrintNumbers.java
@@ -0,0 +1,22 @@
+package CodingChallenge65;
+
+import java.util.Scanner;
+
+public class PrintNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the value of N: ");
+        int N = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++){
+            System.out.print(i);
+            if (i < N){
+                System.out.print(" ");
+            }
+        }
+
+        System.out.println();
+        scanner.close();
+    }
+}
